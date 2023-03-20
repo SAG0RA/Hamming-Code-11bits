8b6865bd106f4ea06cf79af2a0006c3bdecf0caa
Saul <saulgora9@gmail.com>
2023-03-19 23:44:36 -0600

    Terminado con respectivas validaciones

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 import main
 
 
@@ -9,6 +11,7 @@ class HammingEncoder(FloatLayout):
 
 
 class HammingEncoderApp(App):
+    
     def build(self):
         return HammingEncoder()
     
@@ -18,11 +21,19 @@ class HammingEncoderApp(App):
             instancia.text = instancia.text[:-1]
 
     def calcular(self,paridad_text,num_bin):
-        paridad = 0
-        if(paridad_text == 'impar'):
-            paridad = 1
-
-        main.programa(paridad,num_bin)   
-    
-
+        if(paridad_text == 'Paridad'):
+            popup = Popup(title='Alerta', content=Label(text="Agrega una paridad, asegurate de que\n    coincida con el numero binario"), size_hint=(None, None), size=(400, 200))
+            popup.open()
+        else:
+            if(paridad_text == 'par'):
+                paridad = 0
+            else: 
+                paridad = 1
+            
+            if(main.verificar_paridad(num_bin) == paridad):
+                main.programa(paridad,num_bin)
+                self.root.ids.resultado.text = main.convertir_binario_a_hexadecimal(num_bin)
+            else:
+                popup = Popup(title='Alerta', content=Label(text="La paridad no coincide con el numero binario ingresado"), size_hint=(None, None), size=(400, 200))
+                popup.open()
 HammingEncoderApp().run()
